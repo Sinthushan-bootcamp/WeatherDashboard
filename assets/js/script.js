2ec318e528c301c8eae658ff262a88f06cc9617f
sinthushan <sinthushan@gmail.com>
2023-01-24 21:08:46 -0500

    pulled forecast data from api and saved relevant data into object

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var APIKEY = 'e4674172eca777d00a7f445d19b450a9'
+var searchButton = document.getElementById('searchButton')
+var searchInput = document.getElementById('searchInput')
+var weather = []
+function getCityCoordinates() {
+    var city = searchInput.value
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city +'&appid='+ APIKEY;
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        coordinates = {lat: data[0].lat, lon: data[0].lon};
+        getWeatherData(coordinates);
+      });
+}
+
+function getWeatherData(coordinates) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ coordinates.lat +'&lon='+ coordinates.lon +'&appid='+ APIKEY +'&units=metric';
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        forecastData = data.list
+        for (var i = 0; i < forecastData.length; i++) {
+            if (forecastData[i].dt_txt.split(" ")[1] === '12:00:00'){
+                weather.push(
+                    {
+                        date: forecastData[i].dt_txt.split(" ")[0], 
+                        temp: forecastData[i].main.temp,
+                        humidity: forecastData[i].main.humidity,
+                        wind: forecastData[i].wind.speed,
+                        icon: forecastData[i].weather[0].icon
+                    });
+            }
+        }
+        console.log(weather)
+      });
+}
+
+searchButton.addEventListener('click', getCityCoordinates);
